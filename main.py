243e3c86ab6334f7cbbf4fb125f34b568eb9235c
hacim17h <hacim17h@gmail.com>
2024-02-05 21:31:36 -0500

    Import baseline data

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # Name: Micah Calloway Student ID: 010663003
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+# Use a breakpoint in the code line below to debug your script.
+# Press Ctrl+F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+if __name__ == '__main__':
+    dataset = pd.read_csv('top_posts.csv')
+    X = dataset.iloc[:, 3].values
+    y = dataset.iloc[:, -1]
+    print(X)
+    print(y)
+
+
+
